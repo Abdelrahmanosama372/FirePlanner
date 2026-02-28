b12b231f5ba4754f8b3e943d371f8a02409cca13
abdelrahman <abdelrahmanosama372@gmail.com>
2026-02-28 16:08:13 +0200

    Modified Point class - made point default z-axis to be 0 - adjusted serialized json format of class - fixed a typo

--- src/fireplanner/geometry/primitives/point.py
+++ src/fireplanner/geometry/primitives/point.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 from dataclasses import dataclass
-from typing import Any
 from .base import Primitive2D
 from math import sqrt
 
@@ -9,20 +8,17 @@ from math import sqrt
 class Point(Primitive2D):
     x: float
     y: float
-    z: float
+    z: float = 0.0
 
     def distance(self, other: Point):
         return sqrt(
             (self.x - other.x) ** 2 + (self.y - other.y) ** 2 + (self.z - other.z) ** 2
         )
-    
+
     def to_json(self) -> dict[str, str]:
-        return {"Point" : f"{self.x}, {self.y}, {self.z}"} 
+        return {"Point": f"{self.x}, {self.y}, {self.z}"}
 
     @classmethod
     def from_json(cls, data: dict[str, str]) -> Point:
         point_data = [float(i) for i in data["Point"].split(",")]
-        return Point(point_data[0], point_data[1], point_data[2])
-
-
-
+        return Point(x=point_data[0], y=point_data[1], z=point_data[2])
