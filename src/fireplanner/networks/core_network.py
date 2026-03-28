cdf2d980c5e2b96c94b2487ab350062a81d3cf92
abdelrahman <abdelrahmanosama372@gmail.com>
2026-03-28 18:42:50 +0200

    modified core node class to support more useful data core node now provides: - access to node intersection points with or without intersected nodes - access to edges of the node

--- src/fireplanner/networks/core_network.py
+++ src/fireplanner/networks/core_network.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from ..geometry.primitives import Block, Line
+from ..geometry.primitives import Block, Line, Point
 
 
 class CoreNode:
@@ -8,6 +8,8 @@ class CoreNode:
         self._line: Line = line
         self._blocks: list[Block] = []
         self._nodes: list[CoreNode] = []
+        self._intersection_points: dict[Point, CoreNode] = {}
+        self._edges: list[Line] | None = None
 
     @property
     def line(self) -> Line:
@@ -21,17 +23,79 @@ class CoreNode:
     def connected_nodes(self) -> list[CoreNode]:
         return self._nodes
 
+    @property
+    def intersection_points(self) -> dict[Point, CoreNode]:
+        return self._intersection_points
+
+    def find_intersection_points(self) -> list[Point]:
+        return list(self._intersection_points.keys())
+
+    def find_intersection_points_with_nodes(self) -> dict[Point, CoreNode]:
+        return self._intersection_points
+
+    @property
+    def edges(self) -> list[Line]:
+        if self._edges is None:
+            self._edges = self._construct_edges()
+        return self._edges
+
     def add_block(self, block: Block) -> None:
         if isinstance(block, Block):
             self._blocks.append(block)
             return
         raise TypeError("block must be an instance of Block")
 
-    def add_node(self, node: CoreNode) -> None:
-        if isinstance(node, CoreNode):
-            self._nodes.append(node)
-            return
-        raise TypeError("node must be an instance of CoreNode")
+    def add_node(self, node: CoreNode, intersection_point: Point) -> None:
+        if not isinstance(node, CoreNode):
+            raise TypeError("node must be an instance of CoreNode")
+        if not isinstance(intersection_point, Point):
+            raise TypeError("intersection_point must be an instance of Point")
+
+        self._nodes.append(node)
+        self._intersection_points[intersection_point] = node
+        self._edges = None
+
+    def _construct_edges(self) -> list[Line]:
+        sorted_points = sorted(
+            self.find_intersection_points(),
+            key=lambda point: self.line.start.distance(point),
+        )
+
+        edge_points = [self.line.start]
+        for point in sorted_points:
+            if point == edge_points[-1]:
+                continue
+            edge_points.append(point)
+        if edge_points[-1] != self.line.end:
+            edge_points.append(self.line.end)
+
+        edges: list[Line] = []
+        for edge_id, (start, end) in enumerate(
+            zip(edge_points, edge_points[1:]),
+            start=1,
+        ):
+            if start == end:
+                continue
+            edges.append(
+                Line(
+                    id=edge_id,
+                    start=start,
+                    end=end,
+                    line_type=self.line.line_type,
+                )
+            )
+
+        if not edges:
+            return [
+                Line(
+                    id=1,
+                    start=self.line.start,
+                    end=self.line.end,
+                    line_type=self.line.line_type,
+                )
+            ]
+
+        return edges
 
 
 class CoreNetwork:
@@ -75,12 +139,14 @@ class CoreNetwork:
             else:
                 remaining_blocks.append(block)
 
-        child_lines: list[Line] = []
+        child_lines: list[tuple[Line, Point]] = []
         next_remaining_lines: list[Line] = []
         for line in lines:
-            intersects, _ = root_line.intersects_line_2D(line)
+            intersects, intersection_point = root_line.intersects_line_2D(line)
             if intersects:
-                child_lines.append(line)
+                if intersection_point is None:
+                    continue
+                child_lines.append((line, intersection_point))
             else:
                 next_remaining_lines.append(line)
 
@@ -88,7 +154,7 @@ class CoreNetwork:
         # recursing so each branch consumes from the same unvisited set.
         lines[:] = next_remaining_lines
 
-        for child_line in child_lines:
+        for child_line, intersection_point in child_lines:
             child_node = self._create_network_recursive(
                 child_line,
                 lines,
@@ -98,6 +164,6 @@ class CoreNetwork:
             remaining_blocks = [
                 block for block in remaining_blocks if block.id not in used_block_ids
             ]
-            node.add_node(child_node)
+            node.add_node(child_node, intersection_point)
 
         return node
