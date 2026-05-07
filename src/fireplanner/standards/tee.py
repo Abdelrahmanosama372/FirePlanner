a9d908881fc773b4ffb3a76f0e8349e5cf2c329d
abdelrahman <abdelrahmanosama372@gmail.com>
2026-05-07 10:07:06 +0300

    added tee center dimension values

--- src/fireplanner/standards/tee.py
+++ src/fireplanner/standards/tee.py
@@ -1,18 +1,53 @@
 from fireplanner.firecomponent.base import SteelDims
 
-
+# fmt:off
 tee_center_dims = {
-    (SteelDims.DIM_0_5_INCHES,  SteelDims.DIM_0_5_INCHES):  (25, 25),
+    (SteelDims.DIM_0_5_INCHES, SteelDims.DIM_0_5_INCHES): (25, 25),
     (SteelDims.DIM_0_75_INCHES, SteelDims.DIM_0_75_INCHES): (29, 29),
-    (SteelDims.DIM_1_INCHES,    SteelDims.DIM_1_INCHES):   (38, 38),
-    (SteelDims.DIM_1_25_INCHES, SteelDims.DIM_1_25_INCHES):(48, 48),
-    (SteelDims.DIM_1_5_INCHES,  SteelDims.DIM_1_5_INCHES): (57, 57),
-    (SteelDims.DIM_2_INCHES,    SteelDims.DIM_2_INCHES):   (64, 64),
-    (SteelDims.DIM_2_5_INCHES,  SteelDims.DIM_2_5_INCHES): (76, 76),
-    (SteelDims.DIM_3_INCHES,    SteelDims.DIM_3_INCHES):   (86, 86),
-    (SteelDims.DIM_4_INCHES,    SteelDims.DIM_4_INCHES):   (105, 105),
-    (SteelDims.DIM_6_INCHES,    SteelDims.DIM_6_INCHES):   (143, 143),
-    (SteelDims.DIM_8_INCHES,    SteelDims.DIM_8_INCHES):   (178, 178),
-    (SteelDims.DIM_10_INCHES,   SteelDims.DIM_10_INCHES):  (216, 216),
-    (SteelDims.DIM_12_INCHES,   SteelDims.DIM_12_INCHES):  (254, 254),
+
+    (SteelDims.DIM_1_INCHES, SteelDims.DIM_0_5_INCHES): (38, 38),
+    (SteelDims.DIM_1_INCHES, SteelDims.DIM_0_75_INCHES): (38, 38),
+    (SteelDims.DIM_1_INCHES, SteelDims.DIM_1_INCHES): (38, 38),
+
+    (SteelDims.DIM_1_25_INCHES, SteelDims.DIM_0_5_INCHES): (48, 48),
+    (SteelDims.DIM_1_25_INCHES, SteelDims.DIM_0_75_INCHES): (48, 48),
+    (SteelDims.DIM_1_25_INCHES, SteelDims.DIM_1_INCHES): (48, 48),
+    (SteelDims.DIM_1_25_INCHES, SteelDims.DIM_1_25_INCHES): (48, 48),
+
+    (SteelDims.DIM_1_5_INCHES, SteelDims.DIM_0_5_INCHES): (57, 57),
+    (SteelDims.DIM_1_5_INCHES, SteelDims.DIM_0_75_INCHES): (57, 57),
+    (SteelDims.DIM_1_5_INCHES, SteelDims.DIM_1_INCHES): (57, 57),
+    (SteelDims.DIM_1_5_INCHES, SteelDims.DIM_1_25_INCHES): (57, 57),
+    (SteelDims.DIM_1_5_INCHES, SteelDims.DIM_1_5_INCHES): (57, 57),
+
+    (SteelDims.DIM_2_INCHES, SteelDims.DIM_1_INCHES): (64, 51),
+    (SteelDims.DIM_2_INCHES, SteelDims.DIM_1_25_INCHES): (64, 57),
+    (SteelDims.DIM_2_INCHES, SteelDims.DIM_1_5_INCHES): (64, 60),
+    (SteelDims.DIM_2_INCHES, SteelDims.DIM_2_INCHES): (64, 64),
+
+    (SteelDims.DIM_2_5_INCHES, SteelDims.DIM_1_INCHES): (76, 57),
+    (SteelDims.DIM_2_5_INCHES, SteelDims.DIM_1_25_INCHES): (76, 64),
+    (SteelDims.DIM_2_5_INCHES, SteelDims.DIM_1_5_INCHES): (76, 67),
+    (SteelDims.DIM_2_5_INCHES, SteelDims.DIM_2_INCHES): (76, 70),
+    (SteelDims.DIM_2_5_INCHES, SteelDims.DIM_2_5_INCHES): (76, 76),
+
+    (SteelDims.DIM_3_INCHES, SteelDims.DIM_1_INCHES): (86, 67),
+    (SteelDims.DIM_3_INCHES, SteelDims.DIM_1_25_INCHES): (86, 70),
+    (SteelDims.DIM_3_INCHES, SteelDims.DIM_1_5_INCHES): (86, 73),
+    (SteelDims.DIM_3_INCHES, SteelDims.DIM_2_INCHES): (86, 76),
+    (SteelDims.DIM_3_INCHES, SteelDims.DIM_2_5_INCHES): (86, 83),
+    (SteelDims.DIM_3_INCHES, SteelDims.DIM_3_INCHES): (86, 86),
+
+    (SteelDims.DIM_4_INCHES, SteelDims.DIM_1_5_INCHES): (105, 86),
+    (SteelDims.DIM_4_INCHES, SteelDims.DIM_2_INCHES): (105, 89),
+    (SteelDims.DIM_4_INCHES, SteelDims.DIM_3_INCHES): (105, 99),
+    (SteelDims.DIM_4_INCHES, SteelDims.DIM_2_5_INCHES): (105, 95),
+    (SteelDims.DIM_4_INCHES, SteelDims.DIM_4_INCHES): (105, 105),
+
+    (SteelDims.DIM_6_INCHES, SteelDims.DIM_4_INCHES): (143, 130),
+    (SteelDims.DIM_6_INCHES, SteelDims.DIM_6_INCHES): (143, 143),
+
+    (SteelDims.DIM_8_INCHES, SteelDims.DIM_8_INCHES): (178, 178),
+    (SteelDims.DIM_10_INCHES, SteelDims.DIM_10_INCHES): (216, 216),
+    (SteelDims.DIM_12_INCHES, SteelDims.DIM_12_INCHES): (254, 254),
 }
