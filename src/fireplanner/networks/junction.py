afb422de301d1b4b84d99e803ae5986bfbb7684d
abdelrahman <abdelrahmanosama372@gmail.com>
2026-05-08 12:34:16 +0300

    Added junction class to model core network junctions

---
+++ src/fireplanner/networks/junction.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass, field
+from enum import StrEnum
+
+from fireplanner.geometry.primitives import Point
+
+
+class JunctionType(StrEnum):
+    TWO_WAY = "two_way"
+    THREE_WAY = "three_way"
+
+
+@dataclass
+class Junction:
+    id: int
+    origin: Point
+    junction_type: JunctionType | None
+    connected_edges_ids: list[int] = field(default_factory=list)
+    angle: float | None = None
+    has_sprinkler: bool = False
