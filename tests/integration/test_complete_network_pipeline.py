a6b3051cfac3cbcc51f8286135de89c06b96980f
abdelrahman <abdelrahmanosama372@gmail.com>
2026-05-09 13:37:19 +0300

    add: test for a complete actual network

---
+++ tests/integration/test_complete_network_pipeline.py
@@ -0,0 +1,483 @@
+from fireplanner.firecomponent import (
+    Pipe,
+    SteelConnection,
+    SteelDims,
+    SteelMaterial,
+    SteelSchedule,
+    SteelSpecs,
+)
+from math import pi
+from fireplanner.geometry.primitives import Line, Point, Block
+from fireplanner.networks import CoreNetwork, GeometryNetwork, ModelNetwork
+from fireplanner.networks.junction import JunctionType
+
+
+def build_test_network() -> tuple[list[Line], list[Block]]:
+    lines = [
+        Line(id=0, start=Point(x=27.094, y=18.8190), end=Point(x=27.094, y=25.0564)),
+        Line(id=1, start=Point(x=15.0486, y=25.0564), end=Point(x=27.094, y=25.0564)),
+        Line(id=2, start=Point(x=15.0486, y=22.1564), end=Point(x=27.094, y=22.1564)),
+        Line(id=3, start=Point(x=15.0486, y=19.2562), end=Point(x=27.094, y=19.2562)),
+    ]
+
+    blocks = [
+        Block(id=1, name="SPR", center=Point(x=15.0486, y=25.0564)),
+        Block(id=2, name="SPR", center=Point(x=17.9786, y=25.0564)),
+        Block(id=3, name="SPR", center=Point(x=20.9086, y=25.0564)),
+        Block(id=4, name="SPR", center=Point(x=23.8386, y=25.0564)),
+        Block(id=5, name="SPR", center=Point(x=26.7686, y=25.0564)),
+        Block(id=6, name="SPR", center=Point(x=15.0486, y=22.1564)),
+        Block(id=7, name="SPR", center=Point(x=17.9786, y=22.1564)),
+        Block(id=8, name="SPR", center=Point(x=20.9086, y=22.1564)),
+        Block(id=9, name="SPR", center=Point(x=23.8386, y=22.1564)),
+        Block(id=10, name="SPR", center=Point(x=26.7686, y=22.1564)),
+        Block(id=11, name="SPR", center=Point(x=15.0486, y=19.2562)),
+        Block(id=12, name="SPR", center=Point(x=17.9786, y=19.2562)),
+        Block(id=13, name="SPR", center=Point(x=20.9086, y=19.2562)),
+        Block(id=14, name="SPR", center=Point(x=23.8386, y=19.2562)),
+        Block(id=15, name="SPR", center=Point(x=26.7686, y=19.2562)),
+    ]
+    return (lines, blocks)
+
+
+def test_core_network_construction():
+    lines, blocks = build_test_network()
+    core_network = CoreNetwork(lines=lines, sprinkles=blocks)
+
+    assert core_network.get_edges_ids() == [
+        1,
+        2,
+        3,
+        14,
+        15,
+        16,
+        17,
+        18,
+        9,
+        10,
+        11,
+        12,
+        13,
+        4,
+        5,
+        6,
+        7,
+        8,
+    ]
+    assert {
+        edge_id: (
+            line.start.x,
+            line.start.y,
+            line.end.x,
+            line.end.y,
+        )
+        for edge_id, line in core_network.get_lines_with_edge_ids().items()
+    } == {
+        1: (27.094, 18.819, 27.094, 19.2562),
+        2: (27.094, 19.2562, 27.094, 22.1564),
+        3: (27.094, 22.1564, 27.094, 25.0564),
+        14: (27.094, 25.0564, 26.7686, 25.0564),
+        15: (26.7686, 25.0564, 23.8386, 25.0564),
+        16: (23.8386, 25.0564, 20.9086, 25.0564),
+        17: (20.9086, 25.0564, 17.9786, 25.0564),
+        18: (17.9786, 25.0564, 15.0486, 25.0564),
+        4: (27.094, 19.2562, 26.7686, 19.2562),
+        5: (26.7686, 19.2562, 23.8386, 19.2562),
+        6: (23.8386, 19.2562, 20.9086, 19.2562),
+        7: (20.9086, 19.2562, 17.9786, 19.2562),
+        8: (17.9786, 19.2562, 15.0486, 19.2562),
+        9: (27.094, 22.1564, 26.7686, 22.1564),
+        10: (26.7686, 22.1564, 23.8386, 22.1564),
+        11: (23.8386, 22.1564, 20.9086, 22.1564),
+        12: (20.9086, 22.1564, 17.9786, 22.1564),
+        13: (17.9786, 22.1564, 15.0486, 22.1564),
+    }
+    assert {
+        edge_id: core_network.find_edge_sprinkler_count(edge_id)
+        for edge_id in core_network.get_edges_ids()
+    } == {
+        1: 15,
+        2: 10,
+        3: 5,
+        14: 5,
+        15: 4,
+        16: 3,
+        17: 2,
+        18: 1,
+        4: 5,
+        5: 4,
+        6: 3,
+        7: 2,
+        8: 1,
+        9: 5,
+        10: 4,
+        11: 3,
+        12: 2,
+        13: 1,
+    }
+    assert {
+        junction_id: (
+            junction.origin.x,
+            junction.origin.y,
+            junction.junction_type,
+            junction.connected_edges_ids,
+            junction.angle,
+            junction.has_sprinkler,
+        )
+        for junction_id, junction in core_network.get_junctions().items()
+    } == {
+        1: (27.094, 19.2562, JunctionType.THREE_WAY, [1, 2, 4], None, False),
+        2: (27.094, 22.1564, JunctionType.THREE_WAY, [2, 3, 9], None, False),
+        3: (27.094, 25.0564, JunctionType.TWO_WAY, [3, 14], 90.0, False),
+        4: (26.7686, 25.0564, JunctionType.TWO_WAY, [14, 15], 0.0, True),
+        5: (23.8386, 25.0564, JunctionType.TWO_WAY, [15, 16], 0.0, True),
+        6: (20.9086, 25.0564, JunctionType.TWO_WAY, [16, 17], 0.0, True),
+        7: (17.9786, 25.0564, JunctionType.TWO_WAY, [17, 18], 0.0, True),
+        8: (26.7686, 22.1564, JunctionType.TWO_WAY, [9, 10], 0.0, True),
+        9: (23.8386, 22.1564, JunctionType.TWO_WAY, [10, 11], 0.0, True),
+        10: (20.9086, 22.1564, JunctionType.TWO_WAY, [11, 12], 0.0, True),
+        11: (17.9786, 22.1564, JunctionType.TWO_WAY, [12, 13], 0.0, True),
+        12: (26.7686, 19.2562, JunctionType.TWO_WAY, [4, 5], 0.0, True),
+        13: (23.8386, 19.2562, JunctionType.TWO_WAY, [5, 6], 0.0, True),
+        14: (20.9086, 19.2562, JunctionType.TWO_WAY, [6, 7], 0.0, True),
+        15: (17.9786, 19.2562, JunctionType.TWO_WAY, [7, 8], 0.0, True),
+    }
+
+
+def test_model_network_construction():
+    lines, blocks = build_test_network()
+    core_network = CoreNetwork(lines=lines, sprinkles=blocks)
+    # using model network default config (light hazard + auto compute pipe dimensions)
+    model_network = ModelNetwork(core_network)
+
+    assert {
+        edge_id: (
+            pipe.diameter,
+            pipe.material,
+            pipe.schedule,
+            pipe.specs,
+            pipe.connection_type,
+        )
+        for edge_id, pipe in model_network.get_pipes_with_edges_ids().items()
+    } == {
+        1: (
+            SteelDims.DIM_2_5_INCHES,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+        2: (
+            SteelDims.DIM_2_INCHES,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+        3: (
+            SteelDims.DIM_1_5_INCHES,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+        14: (
+            SteelDims.DIM_1_5_INCHES,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+        15: (
+            SteelDims.DIM_1_5_INCHES,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+        16: (
+            SteelDims.DIM_1_25_INCHES,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+        17: (
+            SteelDims.DIM_1_INCHES,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+        18: (
+            SteelDims.DIM_1_INCHES,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+        9: (
+            SteelDims.DIM_1_5_INCHES,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+        10: (
+            SteelDims.DIM_1_5_INCHES,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+        11: (
+            SteelDims.DIM_1_25_INCHES,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+        12: (
+            SteelDims.DIM_1_INCHES,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+        13: (
+            SteelDims.DIM_1_INCHES,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+        4: (
+            SteelDims.DIM_1_5_INCHES,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+        5: (
+            SteelDims.DIM_1_5_INCHES,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+        6: (
+            SteelDims.DIM_1_25_INCHES,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+        7: (
+            SteelDims.DIM_1_INCHES,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+        8: (
+            SteelDims.DIM_1_INCHES,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+    }
+    assert {
+        junction_id: (
+            type(connection).__name__,
+            getattr(connection, "run_diameter", None),
+            getattr(connection, "branch_diameter", None),
+            getattr(connection, "diameter", None),
+            getattr(connection, "angle", None),
+            getattr(connection, "large_diameter", None),
+            getattr(connection, "small_diameter", None),
+            connection.material,
+            connection.schedule,
+            connection.specs,
+            connection.connection_type,
+        )
+        for junction_id, connection in (
+            model_network.get_fire_connections_with_junctions_ids().items()
+        )
+    } == {
+        1: (
+            "Tee",
+            SteelDims.DIM_2_5_INCHES,
+            SteelDims.DIM_1_5_INCHES,
+            None,
+            None,
+            None,
+            None,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+        2: (
+            "Tee",
+            SteelDims.DIM_2_INCHES,
+            SteelDims.DIM_1_5_INCHES,
+            None,
+            None,
+            None,
+            None,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+        3: (
+            "Elbow",
+            None,
+            None,
+            SteelDims.DIM_1_5_INCHES,
+            90.0,
+            None,
+            None,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+        5: (
+            "Reducer",
+            None,
+            None,
+            None,
+            None,
+            SteelDims.DIM_1_5_INCHES,
+            SteelDims.DIM_1_25_INCHES,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+        6: (
+            "Reducer",
+            None,
+            None,
+            None,
+            None,
+            SteelDims.DIM_1_25_INCHES,
+            SteelDims.DIM_1_INCHES,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+        9: (
+            "Reducer",
+            None,
+            None,
+            None,
+            None,
+            SteelDims.DIM_1_5_INCHES,
+            SteelDims.DIM_1_25_INCHES,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+        10: (
+            "Reducer",
+            None,
+            None,
+            None,
+            None,
+            SteelDims.DIM_1_25_INCHES,
+            SteelDims.DIM_1_INCHES,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+        13: (
+            "Reducer",
+            None,
+            None,
+            None,
+            None,
+            SteelDims.DIM_1_5_INCHES,
+            SteelDims.DIM_1_25_INCHES,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+        14: (
+            "Reducer",
+            None,
+            None,
+            None,
+            None,
+            SteelDims.DIM_1_25_INCHES,
+            SteelDims.DIM_1_INCHES,
+            SteelMaterial.ERW,
+            SteelSchedule.SCD40,
+            SteelSpecs.ASTM,
+            SteelConnection.Grooved,
+        ),
+    }
+
+
+def test_geometry_network_construction():
+    lines, blocks = build_test_network()
+    core_network = CoreNetwork(lines=lines, sprinkles=blocks)
+    model_network = ModelNetwork(core_network)
+    geometry_network = GeometryNetwork(core_network, model_network)
+
+    assert {
+        junction_id: (
+            type(component).__name__,
+            component.transform.origin.x,
+            component.transform.origin.y,
+            component.transform.angle,
+        )
+        for junction_id, component in (
+            geometry_network.get_geometric_fire_connections_with_junctions_ids().items()
+        )
+    } == {
+        1: ("GeometricTee", 27.094, 19.2562, pi / 2.0),
+        2: ("GeometricTee", 27.094, 22.1564, pi / 2.0),
+        3: ("GeometricElbow", 597.094, -544.9436, pi / 2.0),
+        5: ("GeometricReducer", -226.16140000000001, 25.0564, 0.0),
+        6: ("GeometricReducer", -229.0914, 25.0564, 0.0),
+        9: ("GeometricReducer", -226.16140000000001, 22.1564, 0.0),
+        10: ("GeometricReducer", -229.0914, 22.1564, 0.0),
+        13: ("GeometricReducer", -226.16140000000001, 19.2562, 0.0),
+        14: ("GeometricReducer", -229.0914, 19.2562, 0.0),
+    }
+    assert {
+        edge_id: (
+            type(pipe).__name__,
+            pipe.start.x,
+            pipe.start.y,
+            pipe.end.x,
+            pipe.end.y,
+        )
+        for edge_id, pipe in geometry_network.get_geometric_pipes_with_edges_ids().items()
+    } == {
+        1: ("GeometricPipe", 27.094, 18.819, 27.094, 19.2562),
+        2: ("GeometricPipe", 27.094, 19.2562, 27.094, 22.1564),
+        3: ("GeometricPipe", 27.094, 22.1564, 27.094, 25.0564),
+        14: ("GeometricPipe", 27.094, 25.0564, 26.7686, 25.0564),
+        15: ("GeometricPipe", 26.7686, 25.0564, 23.8386, 25.0564),
+        16: ("GeometricPipe", 23.8386, 25.0564, 20.9086, 25.0564),
+        17: ("GeometricPipe", 20.9086, 25.0564, 17.9786, 25.0564),
+        18: ("GeometricPipe", 17.9786, 25.0564, 15.0486, 25.0564),
+        9: ("GeometricPipe", 27.094, 22.1564, 26.7686, 22.1564),
+        10: ("GeometricPipe", 26.7686, 22.1564, 23.8386, 22.1564),
+        11: ("GeometricPipe", 23.8386, 22.1564, 20.9086, 22.1564),
+        12: ("GeometricPipe", 20.9086, 22.1564, 17.9786, 22.1564),
+        13: ("GeometricPipe", 17.9786, 22.1564, 15.0486, 22.1564),
+        4: ("GeometricPipe", 27.094, 19.2562, 26.7686, 19.2562),
+        5: ("GeometricPipe", 26.7686, 19.2562, 23.8386, 19.2562),
+        6: ("GeometricPipe", 23.8386, 19.2562, 20.9086, 19.2562),
+        7: ("GeometricPipe", 20.9086, 19.2562, 17.9786, 19.2562),
+        8: ("GeometricPipe", 17.9786, 19.2562, 15.0486, 19.2562),
+    }
